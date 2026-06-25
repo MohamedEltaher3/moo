f199380b89a9119b93e899ba788d16c3e655eb23
Mostafa Eltaher <mohamedeltaher043@gmail.com>
2026-06-25 21:32:15 +0300

    feat: exclude admin from students list

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -10,7 +10,7 @@ const getAllStudents = async(req, res) => {
         }
 
         const { department, name } = req.query;
-        const filter = { isEmailVerified: true };
+        const filter = { isEmailVerified: true, role: "student" };
         if (department) filter.department = department;
         if (name) filter.name = { $regex: name, $options: "i" };
 
